f7cf932292d7174b7ecc102b3bcf3b53e7303ec3
Alicja Halamska <alicja.halamska@allegro.pl>
2021-04-16 14:00:32 +0200

    zad4 - intro

--- src/main/kotlin/com/example/graphqltutorial/TripRepository.kt
+++ src/main/kotlin/com/example/graphqltutorial/TripRepository.kt
@@ -2,6 +2,7 @@ package com.example.graphqltutorial
 
 import com.example.graphqltutorial.model.TripDto
 import org.springframework.stereotype.Service
+import java.time.OffsetDateTime
 
 @Service
 class TripRepository {
@@ -18,10 +19,10 @@ class TripRepository {
 
     private final fun init(): ArrayList<TripDto> {
         val trips = ArrayList<TripDto>()
-        trips.add(TripDto("1", "Paris", "Paris", "Trip to Paris", 12, 1782.20f, "5", listOf("1","2")))
-        trips.add(TripDto("2", "Maledives", "Maledives", "Trip to Maledives", 14,147700.99f, "5", listOf("3","2")))
-        trips.add(TripDto("3", "Russia", "Russia", "Trip to Russia", 43, 23000.00f, "5", listOf("4","1")))
-        trips.add(TripDto("4", "USA", "USA", "Trip to USA", 44, 10000.00f, "5", listOf("3","4")))
+        trips.add(TripDto("1", "Paris", "Paris", "Trip to Paris", 12, 1782.20f, "5", listOf("1","2"), OffsetDateTime.now()))
+        trips.add(TripDto("2", "Maledives", "Maledives", "Trip to Maledives", 14,147700.99f, "5", listOf("3","2"), OffsetDateTime.now()))
+        trips.add(TripDto("3", "Russia", "Russia", "Trip to Russia", 43, 23000.00f, "5", listOf("4","1"), OffsetDateTime.now()))
+        trips.add(TripDto("4", "USA", "USA", "Trip to USA", 44, 10000.00f, "5", listOf("3","4"), OffsetDateTime.now()))
         return trips
     }
 
---
+++ src/main/kotlin/com/example/graphqltutorial/graphql/ScalarTypeConfig.kt
@@ -0,0 +1,42 @@
+package com.example.graphqltutorial.graphql
+
+import graphql.schema.Coercing
+import graphql.schema.CoercingParseLiteralException
+import graphql.schema.CoercingParseValueException
+import graphql.schema.CoercingSerializeException
+import graphql.schema.GraphQLScalarType
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+
+@Configuration
+internal class ScalarTypeConfig {
+    @Bean
+    fun dateTimeScalarType(): GraphQLScalarType {
+        return GraphQLScalarType("DateTime", "An RFC-3339 compliant date time scalar that accepts string values like 1996-12-19T16:39:57-08:00",
+            object : Coercing<OffsetDateTime, String> {
+                @Throws(CoercingSerializeException::class)
+                override fun serialize(dataFetcherResult: Any): String {
+                    return if (dataFetcherResult is OffsetDateTime) {
+                        dataFetcherResult.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                    } else throw CoercingSerializeException(
+                        "Error occurred on serializing class ")
+                }
+
+                @Throws(CoercingParseValueException::class)
+                override fun parseValue(input: Any): OffsetDateTime {
+                    return parse(input)
+                }
+
+                @Throws(CoercingParseLiteralException::class)
+                override fun parseLiteral(input: Any): OffsetDateTime {
+                    return parse(input)
+                }
+
+                protected fun parse(input: Any?): OffsetDateTime {
+                    return OffsetDateTime.parse(input as String?, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                }
+            })
+    }
+}
