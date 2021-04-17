a812d12dc40d351d2770430bd144fcccb78622b6
Alicja Halamska <alicja.halamska@allegro.pl>
2021-04-17 11:48:53 +0200

    zad7 -intro

--- src/main/kotlin/com/example/graphqltutorial/graphql/UserResolver.kt
+++ src/main/kotlin/com/example/graphqltutorial/graphql/UserResolver.kt
@@ -10,51 +10,85 @@ import com.example.graphqltutorial.model.User
 import com.example.graphqltutorial.model.UserCandidate
 import org.springframework.stereotype.Component
 import java.time.LocalDate
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executor
 import java.util.concurrent.TimeUnit
 
 
 @Component
-class UserResolver(val tripRepository: TripRepository, val userRepository: UserRepository): GraphQLResolver<User> {
+class UserResolver(val tripRepository: TripRepository, val userRepository: UserRepository, val executor: Executor) : GraphQLResolver<User> {
 
 
-    fun createdTrips(user: User): List<Trip> {
-        TimeUnit.MILLISECONDS.sleep(100)
-        return tripRepository.findTripsByOwner(user.id).map { dto -> dto.getTrip() }
+    fun createdTrips(user: User): CompletableFuture<List<Trip>> {
+        return CompletableFuture.supplyAsync(
+            {
+                TimeUnit.MILLISECONDS.sleep(100)
+                tripRepository.findTripsByOwner(user.id).map { dto -> dto.getTrip() }
+            },
+            executor::execute)
     }
 
-    fun firstName(user: User): String? {
-        TimeUnit.MILLISECONDS.sleep(100)
-        return userRepository.findUser(user.id)?.firstName
+    fun firstName(user: User): CompletableFuture<String?> {
+        return CompletableFuture.supplyAsync(
+            {
+                TimeUnit.MILLISECONDS.sleep(100)
+                userRepository.findUser(user.id)?.firstName
+            },
+            executor::execute)
     }
 
-    fun surname(user: User): String? {
-        TimeUnit.MILLISECONDS.sleep(100)
-        return userRepository.findUser(user.id)?.surname
+    fun surname(user: User): CompletableFuture<String?> {
+        return CompletableFuture.supplyAsync(
+            {
+                TimeUnit.MILLISECONDS.sleep(100)
+                userRepository.findUser(user.id)?.surname
+            },
+            executor::execute)
     }
 
-    fun email(user: User): String? {
-        TimeUnit.MILLISECONDS.sleep(100)
-        return userRepository.findUser(user.id)?.email
+    fun email(user: User): CompletableFuture<String?> {
+        return CompletableFuture.supplyAsync(
+            {
+                TimeUnit.MILLISECONDS.sleep(100)
+                userRepository.findUser(user.id)?.email
+            },
+            executor::execute)
     }
 
-    fun login(user: User): String? {
-        TimeUnit.MILLISECONDS.sleep(100)
-        return userRepository.findUser(user.id)?.login
+    fun login(user: User): CompletableFuture<String?> {
+        return CompletableFuture.supplyAsync(
+            {
+                TimeUnit.MILLISECONDS.sleep(100)
+                userRepository.findUser(user.id)?.login
+            },
+            executor::execute)
     }
 
-    fun gender(user: User): Gender? {
-        TimeUnit.MILLISECONDS.sleep(100)
-        return userRepository.findUser(user.id)?.gender
+    fun gender(user: User): CompletableFuture<Gender?> {
+        return CompletableFuture.supplyAsync(
+            {
+                TimeUnit.MILLISECONDS.sleep(100)
+                userRepository.findUser(user.id)?.gender
+            },
+            executor::execute)
     }
 
-    fun age(user: User): Int? {
-        TimeUnit.MILLISECONDS.sleep(100)
-        return userRepository.findUser(user.id)?.age
+    fun age(user: User): CompletableFuture<Int?> {
+        return CompletableFuture.supplyAsync(
+            {
+                TimeUnit.MILLISECONDS.sleep(100)
+                userRepository.findUser(user.id)?.age
+            },
+            executor::execute)
     }
 
-    fun birthDate(user: User): LocalDate? {
-        TimeUnit.MILLISECONDS.sleep(100)
-        return userRepository.findUser(user.id)?.birthDate
+    fun birthDate(user: User): CompletableFuture<LocalDate?> {
+        return CompletableFuture.supplyAsync(
+            {
+                TimeUnit.MILLISECONDS.sleep(100)
+                userRepository.findUser(user.id)?.birthDate
+            },
+            executor::execute)
     }
 
 }
