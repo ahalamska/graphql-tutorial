4b5dfd1d47fc6c1d5cfa37c060baa9d41ce7b132
Alicja Halamska <alicja.halamska@allegro.pl>
2021-04-18 15:28:59 +0200

    zad4 -INTRO - repaired

--- src/main/kotlin/com/example/graphqltutorial/graphql/ScalarTypeConfig.kt
+++ src/main/kotlin/com/example/graphqltutorial/graphql/ScalarTypeConfig.kt
@@ -1,5 +1,6 @@
 package com.example.graphqltutorial.graphql
 
+import graphql.language.StringValue
 import graphql.schema.Coercing
 import graphql.schema.CoercingParseLiteralException
 import graphql.schema.CoercingParseValueException
@@ -7,26 +8,25 @@ import graphql.schema.CoercingSerializeException
 import graphql.schema.GraphQLScalarType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.time.OffsetDateTime
-import java.time.format.DateTimeFormatter
+import java.math.BigDecimal
 
 @Configuration
 internal class ScalarTypeConfig {
     @Bean
-    fun dateTimeScalarType(): GraphQLScalarType {
-        return GraphQLScalarType("DateTime", "An RFC-3339 compliant date time scalar that accepts string values like 1996-12-19T16:39:57-08:00",
-            object : Coercing<OffsetDateTime, String> {
+    fun currencyScalarType(): GraphQLScalarType {
+        return GraphQLScalarType("PLNCurrency", "String representing PLN monetary value - decimal with 2 places like 13.42",
+            object : Coercing<BigDecimal, String> {
 
                 @Throws(CoercingSerializeException::class)
                 override fun serialize(dataFetcherResult: Any): String {
-                    return if (dataFetcherResult is OffsetDateTime) {
-                        dataFetcherResult.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                    return if (dataFetcherResult is BigDecimal) {
+                        dataFetcherResult.toDouble().toString()
                     } else throw CoercingSerializeException(
                         "Error occurred on serializing class ")
                 }
 
                 @Throws(CoercingParseValueException::class)
-                override fun parseValue(input: Any): OffsetDateTime {
+                override fun parseValue(input: Any): BigDecimal {
                     try {
                         return parse(input)
                     }
@@ -36,7 +36,7 @@ internal class ScalarTypeConfig {
                 }
 
                 @Throws(CoercingParseLiteralException::class)
-                override fun parseLiteral(input: Any): OffsetDateTime {
+                override fun parseLiteral(input: Any): BigDecimal {
                     try {
                         return parse(input)
                     }
@@ -45,8 +45,22 @@ internal class ScalarTypeConfig {
                     }
                 }
 
-                protected fun parse(input: Any?): OffsetDateTime {
-                    return OffsetDateTime.parse(input as String?, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                protected fun parse(input: Any?): BigDecimal {
+                    return BigDecimal(extractStringValue(input))
+                }
+
+                 private fun extractStringValue(input: Any?) : String?{
+                     return when (input) {
+                         is StringValue -> {
+                             input.value
+                         }
+                         is String -> {
+                             input
+                         }
+                         else -> {
+                             null
+                         }
+                     }
                 }
             })
     }
